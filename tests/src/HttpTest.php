d790a01fe722d9cdbca10f450a8120ea72b29d67
Marcelo Oliveira <marcelo.lopes.oliveira@everis.com>
2018-01-18 13:34:44 -0200

    - testing http-post method

--- tests/src/HttpTest.php
+++ tests/src/HttpTest.php
@@ -1,10 +1,23 @@
 <?php
 
 use PHPUnit\Framework\TestCase;
+use RestfulService\Http;
 
 class HttpTest extends TestCase {
 
-   public function testPost() {
-   
+   public function providerPost() {
+      return array(
+         array('http://www.marceloweb.info/tests/http.php',array('Content-type: application/text'),array('whoareyou'=>'PHPUnit'))
+      );
+   }
+
+   /**
+    * @dataProvider providerPost 
+    */
+   public function testPost($endpoint,$headers,$fields) {
+      $result = Http::post($endpoint,$headers,$fields); 
+      
+      $this->assertEquals($result,'fine');
+
    }
 }
